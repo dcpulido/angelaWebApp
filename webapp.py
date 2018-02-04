6fa87c343eb774a014c78bda1bef8f652fac3215
dcpulido <dcpulido91@gmail.com>
2018-02-04 21:10:35 +0100

    first version

--- webapp.py
+++ webapp.py
@@ -6,6 +6,12 @@ app = Flask(__name__,template_folder="./static")
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+@app.route('/abogacia')
+def abogacia():
+    return render_template("abogacia.html")
+@app.route('/mediacion')
+def mediacion():
+    return render_template("mediacion.html")
 
 
 app.run()
